dd5911ed2a58a24d29264d42936209d7bb07560d
ha0921213 <e94111091@gs.ncku.edu.tw>
2022-11-08 12:55:10 +0000

    lab08.py

--- lab08/lab08.py
+++ lab08/lab08.py
@@ -1,19 +1,20 @@
-import os
-a=os.getcwd()
-b=a.split('/')
-b.remove(b[0])
+#-*- coding: UTF-8 -*-
+import os#呼叫os指令
+a=os.getcwd()#a為當前工作目錄
+b=a.split('/')#用/分開a的東西
+b.remove(b[0])#移除第一個元素
 print(b)
-c='/home/E94111091'
-d=os.listdir(c)
+c='/home/E94111091'#將c設為path
+d=os.listdir(c)#返回path目錄下所有檔案和資料夾
 print(d)
 
-path='E94111091.text'
-f=open(path,'w')
+path='E94111091.text'#建立一個文字檔名
+f=open(path,'w')#寫檔
 for i in range(len(b)):
     f.write(b[i])
-    f.write(os.linesep)
+    f.write(os.linesep)#將b中的元素一個為一行寫入文字檔
 f.write(os.linesep)
 for j in range(len(d)):
     f.write(d[j])
-    f.write(os.linesep)
-f.close()
+    f.write(os.linesep)#將d中的元素一個為一行寫入文字檔
+f.close()#關閉
